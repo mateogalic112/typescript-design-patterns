2a01443cb5666857c47e148d5ba9274e7891d6bf
matteoo.eth <mateogalic112@gmail.com>
2024-10-05 22:38:03 +0200

    update reduce map

--- src/fp/reduceMap.ts
+++ src/fp/reduceMap.ts
@@ -6,6 +6,7 @@ interface Person {
 
 const person = { firstName: "John", lastName: "Doe", age: 20 };
 const person2 = { firstName: "Ann", lastName: "Doe", age: 30 };
+
 const persons = [person, person2];
 
 const concatFullName = (p: Person) => p.firstName + " " + p.lastName;
@@ -13,8 +14,8 @@ const concatFullName = (p: Person) => p.firstName + " " + p.lastName;
 // Using standard `map` function on array
 let fullNames = persons.map(concatFullName);
 
-const mapByReduce = <T extends Person>(items: T[], fn: (item: T) => string) =>
-  items.reduce<string[]>((acc, item) => acc.concat(fn(item)), []);
+const mapByReduce = <T>(items: T[], fn: (item: T) => string) =>
+  items.reduce((acc, item) => acc.concat(fn(item)), [] as string[]);
 
 // Using `reduce` function to implement `map` function
 console.log(mapByReduce(persons, concatFullName)); // [ 'John Doe', 'Ann Doe' ]
