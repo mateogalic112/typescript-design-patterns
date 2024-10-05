a7e9e7938abf6759d2a564a0a9f9d7f08dfd9b03
matteoo.eth <mateogalic112@gmail.com>
2024-10-05 22:43:50 +0200

    update reduce filter

--- src/fp/reduceFilter.ts
+++ src/fp/reduceFilter.ts
@@ -7,20 +7,13 @@ const thug = { name: "John", age: 20 };
 const thug2 = { name: "Ann", age: 30 };
 const thugs = [thug, thug2];
 
-const CLUB_AGE_LIMIT = 21;
-const allowedToClub = (t: Thug) => t.age >= CLUB_AGE_LIMIT;
+const allowedToClub = (t: Thug) => t.age >= 21;
 
 // Using standard `filter` function on array
 const thugsAllowed = thugs.filter(allowedToClub);
 
-const filterByReduce = <T extends Thug>(
-  items: T[],
-  fn: (item: T) => boolean
-) =>
-  items.reduce<Thug[]>(
-    (acc, item) => (fn(item) ? [...acc, item] : acc),
-    []
-  );
+const filterByReduce = <T>(items: T[], fn: (item: T) => boolean) =>
+  items.reduce((acc, item) => (fn(item) ? acc.concat(item) : acc), [] as T[]);
 
 // Using `reduce` function to implement `filter` function
 console.log(filterByReduce(thugs, allowedToClub)); // [ { name: "Ann", age: 30 } ]
