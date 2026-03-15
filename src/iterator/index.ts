bbb0b2cfaf3d0d26ba810d71ae3c05306283b4b3
Mateo Galic <mateogalic112@gmail.com>
2026-03-15 13:06:41 +0100

    update arsenal

--- src/iterator/index.ts
+++ src/iterator/index.ts
@@ -2,9 +2,14 @@ import { Iterator } from "./Iterator";
 import { Weapon } from "./Weapon";
 
 function main() {
-  const arsenal = [new Weapon("AK-47"), new Weapon("M4A1"), new Weapon("AWP")];
+  const arsenal = [
+    new Weapon("AK-47"),
+    new Weapon("M4A1"),
+    new Weapon("AWP"),
+    new Weapon("M249"),
+  ];
 
-  const weapons = new Iterator<Weapon>(arsenal);
+  const weapons = new Iterator(arsenal);
 
   while (weapons.hasNext()) {
     weapons.next().fire();
