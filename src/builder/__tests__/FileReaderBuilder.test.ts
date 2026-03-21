d13f82b4176cc64dce5f7d4eaa92e0a3c810253c
Mateo Galic <mateogalic112@gmail.com>
2026-03-21 18:27:09 +0100

    update tests

--- src/builder/__tests__/FileReaderBuilder.test.ts
+++ src/builder/__tests__/FileReaderBuilder.test.ts
@@ -2,52 +2,50 @@ import { FileProcessor, Preprocessor } from "../FileProcessor";
 import { FileProcessorBuilder } from "../FileProcessorBuilder";
 
 describe("File Processor Builder functionality", () => {
-  describe("Building file processor", () => {
-    const TEXT_ENCODING = "utf-8";
+  const TEXT_ENCODING = "utf-8";
 
-    const toUpperCase: Preprocessor = (data: string) => data.toUpperCase();
+  const toUpperCase: Preprocessor = (data: string) => data.toUpperCase();
 
-    const removeUppercaseWords: Preprocessor = (data: string) =>
-      data.replace(/\b[A-Z]+\b/g, "").trim();
+  const removeUppercaseWords: Preprocessor = (data: string) =>
+    data.replace(/\b[A-Z]+\b/g, "").trim();
 
-    let fileProcessorBuilder: FileProcessorBuilder;
+  let fileProcessorBuilder: FileProcessorBuilder;
 
-    beforeEach(() => {
-      fileProcessorBuilder = new FileProcessorBuilder();
-    });
+  beforeEach(() => {
+    fileProcessorBuilder = new FileProcessorBuilder();
+  });
 
-    test("Should throw if file does not exists", () => {
-      const fileProcessorConstruct = fileProcessorBuilder
-        .setEncoding(TEXT_ENCODING)
-        .addPreprocessor(toUpperCase);
+  test("should throw if file path is not provided", () => {
+    const fileProcessorConstruct = fileProcessorBuilder
+      .setEncoding(TEXT_ENCODING)
+      .addPreprocessor(toUpperCase);
 
-      expect(() => fileProcessorConstruct.build()).toThrowError(
-        "File path is required"
-      );
-    });
+    expect(() => fileProcessorConstruct.build()).toThrowError(
+      "File path is required"
+    );
+  });
 
-    test("Should return FileProcessor instance when only file path is provided", () => {
-      const fileProcessor = fileProcessorBuilder
-        .setFilePath("/some-path")
-        .build();
+  test("should return FileProcessor instance when only file path is provided", () => {
+    const fileProcessor = fileProcessorBuilder
+      .setFilePath("/some-path")
+      .build();
 
-      expect(fileProcessor).toBeInstanceOf(FileProcessor);
-    });
+    expect(fileProcessor).toBeInstanceOf(FileProcessor);
+  });
 
-    test("Should return FileProcessor that applies configured preprocessors", async () => {
-      const filePath = "./src/builder/__tests__/test-file.txt";
+  test("should return FileProcessor that applies configured preprocessors", async () => {
+    const filePath = "./src/builder/__tests__/test-file.txt";
 
-      const fileProcessor = fileProcessorBuilder
-        .setFilePath(filePath)
-        .setEncoding(TEXT_ENCODING)
-        .addPreprocessor(removeUppercaseWords)
-        .addPreprocessor(toUpperCase)
-        .build();
+    const fileProcessor = fileProcessorBuilder
+      .setFilePath(filePath)
+      .setEncoding(TEXT_ENCODING)
+      .addPreprocessor(removeUppercaseWords)
+      .addPreprocessor(toUpperCase)
+      .build();
 
-      const result = await fileProcessor.process();
+    const result = await fileProcessor.process();
 
-      expect(fileProcessor).toBeInstanceOf(FileProcessor);
-      expect(result).toBe("AM JUST A DUMMY FILE!");
-    });
+    expect(fileProcessor).toBeInstanceOf(FileProcessor);
+    expect(result).toBe("AM JUST A DUMMY FILE!");
   });
 });
