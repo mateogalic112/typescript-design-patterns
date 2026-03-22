ce9bc65ecc441df3a8acaa903bb7ba6f9268473a
Mateo Galic <mateogalic112@gmail.com>
2026-03-22 12:52:36 +0100

    update comments

--- src/prototype/index.ts
+++ src/prototype/index.ts
@@ -6,14 +6,15 @@ function main() {
   const sniperClone2 = baseSniper.clone();
   const sniperClone3 = baseSniper.clone();
 
-  sniperClone.attack(100);
-  sniperClone2.attack(300);
-  sniperClone3.attack(200);
-
-  // Executing heavy operation...
-  // Attacking 50
-  // Out of range
-  // Attacking 50
+  sniperClone.attack(100); // Attacking 50
+  sniperClone2.attack(300); // Out of range -> 0
+  sniperClone3.attack(200); // Attacking 50
 }
+
+// Output:
+// Executing heavy operation... (only once, during construction)
+// Attacking 50
+// Out of range
+// Attacking 50
 
 main();
