7930d68c96eb304ca595d5e15aebb608c1a25833
Mateo Galic <mateogalic112@gmail.com>
2026-03-27 21:25:22 +0100

    Remove some lines

--- src/this-keyword/index.ts
+++ src/this-keyword/index.ts
@@ -9,29 +9,23 @@ function main() {
     [AttachmentType.LEFT_HAND, AttachmentType.RIGHT_HAND],
     new Map([[WarriorType.SPARTAN, 101]])
   );
-
   const helmet = new Weapon(
     "helmet",
     20,
     200,
     [AttachmentType.HEAD],
     new Map()
-  );
 
   const spartan = new Warrior(WarriorType.SPARTAN, 100, 100);
   const roman = new Warrior(WarriorType.ROMAN, 100, 100);
-  const persian = new Warrior(WarriorType.PERSIAN, 100, 100);
 
   roman
     .attachWeapon(sword, AttachmentType.LEFT_HAND)
     .attachWeapon(helmet, AttachmentType.HEAD);
-
   spartan.attachWeapon(sword, AttachmentType.LEFT_HAND);
-  persian.attachWeapon(sword, AttachmentType.LEFT_HAND);
 
   console.log({ roman: roman.getWarriorPoints() });
   console.log({ spartan: spartan.getWarriorPoints() });
-  console.log({ persian: persian.getWarriorPoints() });
 }
 
 main();
