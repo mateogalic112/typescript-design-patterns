3db83abd5794404c314572a7bff5ff3f9340b297
Mateo Galic <mateogalic112@gmail.com>
2026-03-28 13:48:39 +0100

    update map reduce signature

--- src/fp/reduceMap.ts
+++ src/fp/reduceMap.ts
@@ -1,29 +1,27 @@
 interface Person {
   firstName: string;
   lastName: string;
-  age: number;
 }
 
-const person = {
+const person_one = {
   firstName: "John",
   lastName: "Doe",
-  age: 20,
 };
-const person2 = {
+const person_two = {
   firstName: "Ann",
   lastName: "Doe",
-  age: 30,
 };
 
-const persons = [person, person2];
+const persons = [person_one, person_two];
 
+// business logic function
 const concatFullName = (p: Person) => p.firstName + " " + p.lastName;
 
 // Using standard `map` function on array
 let fullNames = persons.map(concatFullName);
 
-const mapByReduce = <T>(items: T[], fn: (item: T) => string) =>
-  items.reduce((acc, item) => acc.concat(fn(item)), [] as string[]);
+const mapByReduce = <T, R>(items: T[], fn: (item: T) => R) =>
+  items.reduce<R[]>((acc, item) => acc.concat(fn(item)), []);
 
 // Using `reduce` function to implement `map` function
 console.log(mapByReduce(persons, concatFullName)); // [ 'John Doe', 'Ann Doe' ]
