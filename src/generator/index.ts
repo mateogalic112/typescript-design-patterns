11f307b921d6b45d5d16470c3ec200173a80c610
Mateo Galic <mateogalic112@gmail.com>
2026-03-29 21:41:48 +0200

    Add dynamic data dir

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,17 +1,20 @@
+import path from "path";
 import { WeaponProcessor } from "./WeaponProcessor";
 import { LineReader } from "./LineReader";
 import { CityProcessor } from "./CityProcessor";
+
+const DATA_DIR = path.resolve(__dirname, "..", "..", "src", "generator");
 
 async function main() {
   const lineReader = new LineReader();
 
   const cityProcessor = new CityProcessor(lineReader);
-  for await (const city of cityProcessor.processData("src/generator/cities.txt")) {
+  for await (const city of cityProcessor.processData(path.resolve(DATA_DIR, "cities.txt"))) {
     console.log("City:", city);
   }
 
   const weaponProcessor = new WeaponProcessor(lineReader);
-  for await (const weapon of weaponProcessor.processData("src/generator/weapons.txt")) {
+  for await (const weapon of weaponProcessor.processData(path.resolve(DATA_DIR, "weapons.txt"))) {
     console.log("Weapon:", weapon);
   }
 }
