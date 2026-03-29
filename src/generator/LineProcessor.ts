33b25a3560930562093232beda7e5dc82fb6909a
Mateo Galic <mateogalic112@gmail.com>
2026-03-29 20:59:48 +0200

    Update abstract method args

--- src/generator/LineProcessor.ts
+++ src/generator/LineProcessor.ts
@@ -3,5 +3,5 @@ import { LineReader } from "./LineReader";
 export abstract class LineProcessor<T> {
   constructor(protected lineReader: LineReader) {}
 
-  abstract processData(line: string): Promise<T>;
+  abstract processData(path: string): Promise<T>;
 }
